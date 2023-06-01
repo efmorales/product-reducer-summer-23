a57fad5e58b59415df907a6a829385fb725f68e5
efmorales <54375116+efmorales@users.noreply.github.com>
2023-06-01 12:26:51 -0400

    added all-products route and array

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -6,4 +6,44 @@ router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
+router.get('/all-products', (req, res, next) => {
+    res.send([
+        {
+          "id": 782330,
+          "title": "Doom: Eternal",
+          "publisher": "Bethesda",
+          "genre": "FPS",
+          "MSRP": 39.99
+        },
+        {
+          "id": 546560,
+          "title": "Half-Life: Alyx",
+          "publisher": "Valve",
+          "genre": "VR",
+          "MSRP": 59.99
+        },
+        {
+          "id": 1145360,
+          "title": "Hades",
+          "publisher": "Supergiant",
+          "genre": "Rogue-like",
+          "MSRP": 24.99
+        },
+        {
+          "id": 2050650,
+          "title": "Resident Evil 4",
+          "publisher": "Capcom",
+          "genre": "Horror",
+          "MSRP": 59.99
+        },
+        {
+          "id": 2208920,
+          "title": "Assassins Creed: Valhalla",
+          "publisher": "Ubisoft",
+          "genre": "Third Person",
+          "MSRP": 59.99
+        }
+     ])
+})
+
 module.exports = router;
